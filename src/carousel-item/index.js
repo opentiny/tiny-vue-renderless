0f267689bb41d4c112da834f89d07fb8cc5cb10e
shenjunjian <40288193@qq.com>
2023-03-06 23:52:29 -0800

    fix(carousel): 修复走马灯组件点击后，只跳转到最后一项的问题

--- src/carousel-item/index.js
+++ src/carousel-item/index.js
@@ -1,14 +1,14 @@
 /**
-* Copyright (c) 2022 - present TinyVue Authors.
-* Copyright (c) 2022 - present Huawei Cloud Computing Technologies Co., Ltd.
-*
-* Use of this source code is governed by an MIT-style license.
-*
-* THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
-* BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
-* A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
-*
-*/
+ * Copyright (c) 2022 - present TinyVue Authors.
+ * Copyright (c) 2022 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
 
 export const processIndex = ({ activeIndex, index, length }) => {
   if (activeIndex === 0 && index === length - 1) {
@@ -73,7 +73,7 @@ export const handleItemClick = (parent) => () => {
   const vnode = parent.$parent
 
   if (vnode && vnode.type === parent.$constants.TYPE_CARD) {
-    const index = vnode.state.items.indexOf(parent)
+    const index = vnode.state.items.findIndex((item) => item.$el === parent.$el)
     vnode.setActiveItem(index)
   }
 }
